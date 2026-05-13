b9e6da2faae832a58f513e2e5a72a5b870aeb305
Lee Davis <119569256+otrw@users.noreply.github.com>
2026-05-13 09:06:30 +0100

    feat: added added input validation for password type and number of passwords

--- dino_pass.py
+++ dino_pass.py
@@ -1,11 +1,17 @@
 import requests
 # import json # Uncomment if you want to print JSON formatted output
 
-# TODO: Change to command line arguments
+# TODO: Change to command line arguments using argparse
 no_of_passwords = 10
 password_type = 'simple'
 
-# TODO: Validate user input and response data
+# Validate input parameters
+if no_of_passwords < 1 or no_of_passwords > 300:
+    raise ValueError("no_of_passwords must be between 1 and 300")
+
+if password_type not in ['simple', 'strong']:
+    raise ValueError("password_type must be 'simple' or 'strong'")
+
 response = requests.get(f"https://www.dinopass.com/password/{password_type}?n={no_of_passwords}&format=json", timeout=10)
 
 response.raise_for_status() # Raises an HTTP Error if the request returned an unsuccessful status code
@@ -16,9 +22,13 @@ data = response.json()
 if "passwords" not in data:
     raise ValueError("Unexpected response")
 
+# Validate that the count field is present
+if "count" not in data:
+    raise ValueError("Unexpected response: missing count")
+
 # Validate the number of passwords returned
 if data["count"] != no_of_passwords:
-    raise ValueError("Warning: fewer passwords returned than requested")
+    raise ValueError("API returned fewer passwords than requested")
 
 passwords_list = data['passwords']
 
