a1b135f65255826c0641a9fb88a8d822587b5cf1
Lee Davis <119569256+otrw@users.noreply.github.com>
2026-05-13 11:29:07 +0100

    feat: added command line arguments using argparse

--- dino_pass.py
+++ dino_pass.py
@@ -1,18 +1,27 @@
 import requests
-# import json # Uncomment if you want to print JSON formatted output
+import argparse
 
-# TODO: Change to command line arguments using argparse
-no_of_passwords = 10
-password_type = 'simple'
+parser = argparse.ArgumentParser(
+    description="Generate random passwords using the DinoPass API.",
+    epilog="Example: python dino_pass.py -n 5 -t strong"
+    )
+
+parser.add_argument('-n', '--number', type=int, default=10, help='Number of passwords to generate (1-300)')
+parser.add_argument('-t', '--type', type=str, default='simple', choices=['simple', 'strong'], help='Type of passwords to generate (simple or strong)') 
+args = parser.parse_args()
 
 # Validate input parameters
-if no_of_passwords < 1 or no_of_passwords > 300:
-    raise ValueError("no_of_passwords must be between 1 and 300")
+if args.type == "simple":
+    max_passwords = 300
+else:  # strong
+    max_passwords = 10
 
-if password_type not in ['simple', 'strong']:
-    raise ValueError("password_type must be 'simple' or 'strong'")
+if args.number < 1 or args.number > max_passwords:
+    raise ValueError(
+        f"For '{args.type}' passwords, number must be between 1 and {max_passwords}"
+    )
 
-response = requests.get(f"https://www.dinopass.com/password/{password_type}?n={no_of_passwords}&format=json", timeout=10)
+response = requests.get(f"https://www.dinopass.com/password/{args.type}?n={args.number}&format=json", timeout=10)
 
 response.raise_for_status() # Raises an HTTP Error if the request returned an unsuccessful status code
 
@@ -20,19 +29,13 @@ data = response.json()
 
 # Validate the response structure
 if "passwords" not in data:
-    raise ValueError("Unexpected response")
+    raise ValueError("Unexpected response, no passwords field found")
 
-# Validate that the count field is present
-if "count" not in data:
-    raise ValueError("Unexpected response: missing count")
-
-# Validate the number of passwords returned
-if data["count"] != no_of_passwords:
+# Validate the number of passwords returned by the API
+if data["count"] != args.number:
     raise ValueError("API returned fewer passwords than requested")
 
 passwords_list = data['passwords']
 
 # TODO: Add options for output to text file
 print("\n".join(passwords_list))
-
-# print(json.dumps(data, indent=4, sort_keys=True)) # Debug: Pretty print JSON response
