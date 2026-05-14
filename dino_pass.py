6586c44bea52aa80fc86b95013a5361dcc422360
Lee Davis <119569256+otrw@users.noreply.github.com>
2026-05-14 08:43:53 +0100

    feat: Added argparse.ArgumentDefaultsHelpFormatter to show default values in help message

--- dino_pass.py
+++ dino_pass.py
@@ -3,13 +3,13 @@ import argparse
 
 parser = argparse.ArgumentParser(
     description="Generate random passwords using the DinoPass API.",
-    epilog="Example: python dino_pass.py -n 5 -t strong"
+    epilog="Example: python dino_pass.py -n 5 -t simple",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-# TODO: Add usage as default
-# TODO: Add usuage help with -h or --help and example usage in the help message
-parser.add_argument('-n', '--number', type=int, default=10, help='Number of passwords to generate. Simple 1-300, Strong 1-10')
-parser.add_argument('-t', '--type', type=str, default='simple', choices=['simple', 'strong'], help='Type of passwords to generate (simple or strong)')
-parser.add_argument('-f', '--file', type=str, help='Output file to save the passwords (optional)') 
+
+parser.add_argument('-n', '--number', type=int, default=10, help='Number of passwords to generate. Simple  Max: 300, Strong Max: 10.')
+parser.add_argument('-t', '--type', type=str, default='simple', choices=['simple', 'strong'], help='Password type simple or strong.')
+parser.add_argument('-f', '--file', type=str, help='Output file to save the passwords (optional)')
 args = parser.parse_args()
 
 # Validate input parameters
